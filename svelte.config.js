f149c3a4a7be65cbe732e6b2443012229be26893
Oleg Polin <140200108+olegpolin@users.noreply.github.com>
2026-02-25 13:59:55 -0500

    Add mdsvex

--- svelte.config.js
+++ svelte.config.js
@@ -1,3 +1,4 @@
+import { mdsvex } from 'mdsvex';
 import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -7,7 +8,9 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter()
-	}
+	},
+	preprocess: [mdsvex()],
+	extensions: ['.svelte', '.svx']
 };
 
 export default config;
