62d34c41d751373f5da13b0e959202e00d3ce9c8
Oleg Polin <140200108+olegpolin@users.noreply.github.com>
2026-02-26 16:40:43 -0500

    Add mode watcher

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import { ModeWatcher } from 'mode-watcher';
 	import { Toaster } from '$lib/components/ui/sonner';
 
 	let { children } = $props();
@@ -8,6 +9,8 @@
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
 
-<Toaster />
+<ModeWatcher defaultMode="dark" />
+
+<Toaster richColors />
 
 {@render children()}
