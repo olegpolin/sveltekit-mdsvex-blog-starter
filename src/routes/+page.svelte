8da161bd3b3d224881c905fcdce05106edd45755
Oleg Polin <140200108+olegpolin@users.noreply.github.com>
2026-03-01 09:56:03 -0500

    Add link to blog page in homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,16 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+  import * as Card from '$lib/components/ui/card';
+  import { Button } from '$lib/components/ui/button';
+</script>
+
+<div class="grow flex items-center justify-center">
+  <Card.Root class="w-full max-w-96">
+    <Card.Header>
+      <Card.Title>Blog</Card.Title>
+      <Card.Description>This blog implementation uses MDsveX.</Card.Description>
+    </Card.Header>
+    <Card.Footer>
+      <Button class="w-full" href="/blog">View posts</Button>
+    </Card.Footer>
+  </Card.Root>
+</div>
