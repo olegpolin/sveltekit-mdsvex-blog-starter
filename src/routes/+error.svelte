56c698fc2bdd3a0d2115fc61ab9b4b7a94207063
Oleg Polin <140200108+olegpolin@users.noreply.github.com>
2026-03-01 14:50:46 -0500

    Add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import { page } from '$app/state';
+</script>
+
+<div class="grow flex flex-col items-center justify-center gap-4">
+  <h1 class="text-4xl text-center font-semibold">{page.status}</h1>
+  <p class="text-2xl text-center">{page.error?.message}</p>
+</div>
