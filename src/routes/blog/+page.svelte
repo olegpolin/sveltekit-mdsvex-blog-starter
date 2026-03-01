fa508079ee150af75552f0acb0fb9966033d83fe
Oleg Polin <140200108+olegpolin@users.noreply.github.com>
2026-03-01 13:45:23 -0500

    Add title to blog page

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -6,23 +6,26 @@
   let { data }: PageProps = $props();
 </script>
 
-<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-  {#each data.posts as post}
-    <Card.Root>
-      <Card.Header>
-        <Card.Title>{post.title}</Card.Title>
-        <Card.Description>
-          {new Date(post.date).toLocaleDateString('en-US', {
-            year: 'numeric',
-            month: 'short',
-            day: 'numeric'
-          })}
-        </Card.Description>
-      </Card.Header>
-      <Card.Content>{post.description}</Card.Content>
-      <Card.Footer class="mt-auto">
-        <Button class="w-full" href="/blog/{post.slug}">Read more</Button>
-      </Card.Footer>
-    </Card.Root>
-  {/each}
+<div class="flex flex-col gap-8">
+  <h1 class="text-4xl font-semibold">Latest Posts</h1>
+  <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+    {#each data.posts as post}
+      <Card.Root>
+        <Card.Header>
+          <Card.Title class="text-2xl">{post.title}</Card.Title>
+          <Card.Description>
+            {new Date(post.date).toLocaleDateString('en-US', {
+              year: 'numeric',
+              month: 'short',
+              day: 'numeric'
+            })}
+          </Card.Description>
+        </Card.Header>
+        <Card.Content>{post.description}</Card.Content>
+        <Card.Footer class="mt-auto">
+          <Button class="w-full" href="/blog/{post.slug}">Read more</Button>
+        </Card.Footer>
+      </Card.Root>
+    {/each}
+  </div>
 </div>
