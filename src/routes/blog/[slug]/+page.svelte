61900e0e2a52a98aab5a5f7ca9683564d90bec78
Oleg Polin <140200108+olegpolin@users.noreply.github.com>
2026-03-01 17:52:26 -0500

    Add back to blog button on post page

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
   import type { PageProps } from './$types';
+  import { Button } from '$lib/components/ui/button';
   import { formatDate } from '$lib/utils';
+  import ArrowLeftIcon from '@lucide/svelte/icons/arrow-left';
 
   let { data }: PageProps = $props();
   let Content = $derived(data.content);
@@ -11,8 +13,15 @@
 	<meta name="description" content="SvelteKit + MDsveX blog starter template with Tailwind CSS and shadcn-svelte" />
 </svelte:head>
 
-<article class="prose prose-zinc dark:prose-invert mx-auto">
-  <h1>{data.meta.title}</h1>
-  <p class="text-sm text-muted-foreground">{formatDate(data.meta.date)}</p>
-  <Content />
-</article>
+<div class="flex flex-col items-center gap-16">
+  <article class="prose prose-zinc dark:prose-invert mx-auto">
+    <h1>{data.meta.title}</h1>
+    <p class="text-sm text-muted-foreground">{formatDate(data.meta.date)}</p>
+    <Content />
+  </article>
+
+  <Button variant="outline" href="/blog">
+    <ArrowLeftIcon />
+    Back to Blog
+  </Button>
+</div>
